ad122ab4b1629ddfa7733f318b88c0218f7d90a3
bilal asghar <asgharbilal8@gmail.com>
2022-01-29 15:51:44 +0000

    created database helper for connection and queries to database

---
+++ src/MasterPages/DatabaseHelpers.php
@@ -0,0 +1,43 @@
+<?php
+function OpenConnection()
+{
+    $server = "localhost";
+    $username = "root";
+    $password = "Hoisaejfr^&o2";
+    $database = "ExpenseTracker";
+
+    $conn = new mysqli($server, $username, $password, $database);
+
+    if ($conn === false) {
+        die("ERROR: Could not connect. " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+function CallDatabase($sql, $isDataReturned)
+{
+    try {
+        $conn = OpenConnection();
+
+        if ($isDataReturned) {
+            $result = mysqli_query($conn, $sql);
+
+            // we have data so store in variable then return
+
+            while ($row = mysqli_fetch_array($result)) {
+                $rows[] = $row;
+            }
+
+            mysqli_free_result($result); // free memory associated with result
+            $conn->close();
+            return $rows;
+        } else {
+            $result = mysqli_query($conn, $sql);
+            $conn->close();
+        }
+    } catch (Exception $e) {
+        $conn->close();
+        echo "Error!" . $e->getMessage();
+    }
+}
