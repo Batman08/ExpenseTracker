714a0ee305866dbc455d781d9eb999997da8be20
bilal asghar <asgharbilal8@gmail.com>
2022-01-29 15:45:05 +0000

    create login page

---
+++ src/Login/Login.php
@@ -0,0 +1,5 @@
+<?php
+$page_header = '';
+$page_content = 'Login_Content.php';
+include('../MasterPages/Master.php');
+?>
---
+++ src/Login/Login_Content.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card">
+            <div class="card-header">
+                <h3><i class="fas fa-sign-in-alt"></i> Login</h3>
+            </div>
+            <div class="card-body">
+                <form action='' method='POST'>
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" class="form-control" name="txtUsername" placeholder="Enter Username" maxlength="256" required>
+                    </div>
+                    <div class="form-group" style="margin-top: 10px;">
+                        <label for="username">Password:</label>
+                        <input type="password" class="form-control" name="txtPassword" placeholder="Enter Password" maxlength="256" required>
+                    </div>
+                    <div class="form-group" style="margin-top: 20px;">
+                        <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-sign-in-alt"></i> Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
