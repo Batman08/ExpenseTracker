7aee9bde50789a1ba44b7b6b54ae035722086557
bilal asghar <asgharbilal8@gmail.com>
2022-01-31 22:58:09 +0000

    added code to put username and user id into session

--- src/Login/Login_Authenticate.php
+++ src/Login/Login_Authenticate.php
@@ -1,11 +1,18 @@
 <?php 
+    session_start();
+
     include("../MasterPages/DatabaseHelpers.php");
 
     $username =  $_POST["txtUsername"];
     $password =  $_POST["txtPassword"];
     $loginDetails = GetUserLoginDetails($username);
 
+    
+
     if ($username == $loginDetails["Username"] && $password ==  $loginDetails["Password"]){
+        $_SESSION["username"] = $username;
+        $_SESSION["userId"] = GetUserId($username);
+
         header("Location: ../UserExpenses/TrackExpenses.php");
     }
     else {
