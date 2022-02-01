d9adba7ec1dbb752507386341b31de2003669162
bilal asghar <asgharbilal8@gmail.com>
2022-02-01 22:31:27 +0000

    added ajax include to master page

--- src/MasterPages/Master.php
+++ src/MasterPages/Master.php
@@ -1,3 +1,10 @@
+<?php 
+    session_start();
+    // if ($_SESSION["medicalPersonId"] == null && basename($_SERVER['PHP_SELF']) != "Login.php") {
+    //     header('Location: ../Login/Login.php');
+    // }
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -38,8 +45,12 @@
         </footer>
     </div>
 
+    <?php include("Ajax.php"); ?>
+
     <!-- External Javascript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
