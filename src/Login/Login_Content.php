0d269ea408862dcc0688f1c45b6211ce9505a159
bilal asghar <asgharbilal8@gmail.com>
2022-02-12 23:46:12 +0000

    Added project title to login page

--- src/Login/Login_Content.php
+++ src/Login/Login_Content.php
@@ -1,6 +1,11 @@
+<div class="row" style="margin-bottom: 50px;">
+    <div class="col-md-6 mx-auto">
+        <h1 class="display-4">Expense Tracker</h1>
+    </div>
+</div>
 <div class="row">
     <div class="col-md-6 mx-auto">
-    <div id="divLoginMessage"></div>
+        <div id="divLoginMessage"></div>
         <div class="card">
             <div class="card-header">
                 <h3><i class="fas fa-sign-in-alt"></i> Login</h3>
