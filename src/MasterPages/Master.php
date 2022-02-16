fe3b079168496deab5607e5792dbfec6e5bf57c0
bilal asghar <asgharbilal8@gmail.com>
2022-02-16 23:51:49 +0000

    renamed php file that includes javascript code

--- src/MasterPages/Master.php
+++ src/MasterPages/Master.php
@@ -54,7 +54,7 @@ if ($_SESSION["userId"] == null ) {
         </footer>
     </div>
 
-    <?php include("Ajax.php"); ?>
+    <?php include("Javascript_Include.php"); ?>
 
     <!-- External Javascript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
